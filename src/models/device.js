4f378a862653a29ed4355507c0b709e080112468
ngocrc <tienngoc2003dh@gmail.com>
2024-06-08 22:37:43 +0700

    update models

--- src/models/device.js
+++ src/models/device.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const fansSchema = new mongoose.Schema ({
-    userId : String,
-    name : String,
+    userId : {type : String, default : ""},
+    name : {type : String, default : ""},
     state : {type : Number, default : 0},
     level : {type : Number, default : 0, min : 0, max : 2} 
 }, {timestamps:true})
@@ -12,8 +12,8 @@ fansSchema.virtual("type").get(function() {return "fan"})
 module.exports.Fan = mongoose.model('fans', fansSchema)
 
 const ledsSchema = new mongoose.Schema ({
-    userId : String,
-    name : String,
+    userId : {type : String, default : ""},
+    name : {type : String, default : ""},
     state : {type : Number, default : 0},
     schedule : {
         time : {type : Date, default : new Date(0, 0, 1, 0, 0, 0, 0)},
@@ -26,8 +26,8 @@ ledsSchema.virtual("type").get(function() {return "led"})
 module.exports.Led = mongoose.model('leds', ledsSchema)
 
 const doorsSchema = new mongoose.Schema ({
-    userId : String,
-    name : String,
+    userId : {type : String, default : ""},
+    name : {type : String, default : ""},
     state : {type : Number, default : 0},
 }, {timestamps:true})
 
